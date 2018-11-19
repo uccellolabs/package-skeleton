6f886bcd130ebd74ee1b57004c01a1838a9baf27
Jonathan SARDO <sardoj@gmail.com>
2018-11-19 12:03:42 -0400

    feat: Adds soft deletes and table prefix

--- app/Models/ModuleSkeleton.php
+++ app/Models/ModuleSkeleton.php
@@ -2,16 +2,31 @@
 
 namespace Uccello\PackageSkeleton\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Uccello\Core\Database\Eloquent\Model;
 
 class ModuleSkeleton extends Model
 {
+    use SoftDeletes;
+
     /**
      * The table associated with the model.
      *
      * @var string
      */
-    protected $table = 'module_skeletons';
+    protected $table = 'table_name';
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    protected function setTablePrefix()
+    {
+        $this->tablePrefix = 'table_prefix';
+    }
 
     /**
      * Returns record label
