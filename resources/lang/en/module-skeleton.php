7aea3b7b76865acc5adbce8a8409726089f45f69
Jonathan SARDO <sardoj@gmail.com>
2018-12-13 21:46:42 +0100

    feat: Removes module files (no longer necessary)

--- resources/lang/en/module-skeleton.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'module-skeleton' => 'Module Skeletons',
-    'module-skeleton.single' => 'Module Skeleton',
-    'field.name' => 'Name',
-    // Other translations (tabs, blocks, fields, ...)
-];
